ad6396579388b3e33dacfd5bdb3ce3827dbe997d
Lance Robson <lance@tigera.io>
2016-09-22 12:32:23 +0100

    Fix bugs in metabot3.py (by JP)

--- metabot3.py
+++ metabot3.py
@@ -5,22 +5,22 @@ import time
 def apply_wheel_velocities(
         forward_back_vel=0.0, left_right_vel=0.0, rot_vel=0.0):
     # Apply the desired wheel velocities:
-    vrep.simxSetJointTargetVelocity(
-        clientID, wheel_joints[1],
+    ret = vrep.simxSetJointTargetVelocity(
+        clientID, wheel_joints[0],
         -forward_back_vel - left_right_vel - rot_vel,
-        vrep.simx_opmode_oneshot)
-    vrep.simxSetJointTargetVelocity(
-        clientID, wheel_joints[2],
+        vrep.simx_opmode_blocking)
+    ret = vrep.simxSetJointTargetVelocity(
+        clientID, wheel_joints[1],
         -forward_back_vel + left_right_vel - rot_vel,
-        vrep.simx_opmode_oneshot)
-    vrep.simxSetJointTargetVelocity(
-        clientID, wheel_joints[3],
+        vrep.simx_opmode_blocking)
+    ret = vrep.simxSetJointTargetVelocity(
+        clientID, wheel_joints[2],
         -forward_back_vel - left_right_vel + rot_vel,
-        vrep.simx_opmode_oneshot)
-    vrep.simxSetJointTargetVelocity(
-        clientID, wheel_joints[4],
+        vrep.simx_opmode_blocking)
+    ret = vrep.simxSetJointTargetVelocity(
+        clientID, wheel_joints[3],
         -forward_back_vel + left_right_vel + rot_vel,
-        vrep.simx_opmode_oneshot)
+        vrep.simx_opmode_blocking)
 
 
 def main():
@@ -33,8 +33,8 @@ def main():
     # Stop
     apply_wheel_velocities()
 
-    import pdb
-    pdb.set_trace()
+    #import pdb
+    #pdb.set_trace()
 
 
 print 'Program started'
@@ -42,31 +42,31 @@ vrep.simxFinish(-1)  # just in case, close all opened connections
 clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
 if clientID != -1:
     print 'Connected to remote API server'
-    vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
+    ret = vrep.simxStartSimulation(clientID, vrep.simx_opmode_blocking)
 
     # Get joints (motors) from vrep
     # front left, rear left, rear right, front right
     wheel_joints = [-1, -1, -1, -1]
-    wheel_joints[1] = vrep.simxGetObjectHandle(
-        clientID, 'rollingJoint_fl', vrep.simx_opmode_oneshot)
-    wheel_joints[2] = vrep.simxGetObjectHandle(
-        clientID, 'rollingJoint_rl', vrep.simx_opmode_oneshot)
-    wheel_joints[3] = vrep.simxGetObjectHandle(
-        clientID, 'rollingJoint_rr', vrep.simx_opmode_oneshot)
-    wheel_joints[4] = vrep.simxGetObjectHandle(
-        clientID, 'rollingJoint_fr', vrep.simx_opmode_oneshot)
-    youBot = vrep.simxGetObjectHandle(
-        clientID, 'youBot', vrep.simx_opmode_oneshot)
-    youBotRef = vrep.simxGetObjectHandle(
-        clientID, 'youBot_ref', vrep.simx_opmode_oneshot)
-    tip = vrep.simxGetObjectHandle(
-        clientID, 'youBot_positionTip', vrep.simx_opmode_oneshot)
-    target = vrep.simxGetObjectHandle(
-        clientID, 'youBot_positionTarget', vrep.simx_opmode_oneshot)
+    ret, wheel_joints[0] = vrep.simxGetObjectHandle(
+        clientID, 'rollingJoint_fl', vrep.simx_opmode_blocking)
+    ret, wheel_joints[1] = vrep.simxGetObjectHandle(
+        clientID, 'rollingJoint_rl', vrep.simx_opmode_blocking)
+    ret, wheel_joints[2] = vrep.simxGetObjectHandle(
+        clientID, 'rollingJoint_rr', vrep.simx_opmode_blocking)
+    ret, wheel_joints[3] = vrep.simxGetObjectHandle(
+        clientID, 'rollingJoint_fr', vrep.simx_opmode_blocking)
+    ret, youBot = vrep.simxGetObjectHandle(
+        clientID, 'youBot', vrep.simx_opmode_blocking)
+    ret, youBotRef = vrep.simxGetObjectHandle(
+        clientID, 'youBot_ref', vrep.simx_opmode_blocking)
+    ret, tip = vrep.simxGetObjectHandle(
+        clientID, 'youBot_positionTip', vrep.simx_opmode_blocking)
+    ret, target = vrep.simxGetObjectHandle(
+        clientID, 'youBot_positionTarget', vrep.simx_opmode_blocking)
 
     main()
 
-    vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
+    ret = vrep.simxStopSimulation(clientID, vrep.simx_opmode_blocking)
     vrep.simxFinish(clientID)
 else:
     print 'Failed connecting to remote API server'
